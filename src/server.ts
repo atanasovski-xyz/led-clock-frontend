47bdf01442d49964ef028a5e078169055368bcd0
Daniel Atanasovski <git@danielatanasovski.com>
2025-01-04 04:50:57 +0000

    fix: removal of sentry

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import path from "path";
-import * as Sentry from "@sentry/browser";
 
 // Initialize Express app
 const app = express();
-
-Sentry.init({
-  dsn: "https://77a0d8b7353f4e31b2b8c0fd27084f4a@glitchtip.atanasovski.xyz/2",
-  integrations: [Sentry.browserSessionIntegration()],
-});
 
 // Use body-parser middleware to parse JSON requests
 app.use(bodyParser.json());
